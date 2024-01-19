1f59f982ebb85e3a523922aacae8a693bb5d1cfb
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-01-19 19:35:57 +0200

    ex 1 - tema 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
 
-console.log(galleryItems);
+
+//Declaration
+const body = document.body
+const container =  document.querySelector(".gallery");
+const fragment = document.createDocumentFragment();
+let max = galleryItems.length;
+
+//Function
+function liImages()
+{
+  for(let i = 0; i < max; i++)
+    {let itemLi = document.createElement("li");
+     itemLi.classList.add("gallery__item");
+     let itemA = document.createElement("a");
+     itemA.classList.add("gallery__link");
+     let liImg = document.createElement("img");
+     liImg.classList.add("gallery__image");
+
+  let basic = galleryItems[i];
+  for(let item of galleryItems){
+     liImg.setAttribute("src", `${basic.preview}`);
+     itemA.setAttribute("href", `${basic.original}`);
+     liImg.setAttribute("data-source", `${basic.original}`);
+     liImg.setAttribute("alt", `${basic.description}`);}
+
+     itemLi.append(itemA);
+     itemA.append(liImg);
+     fragment.append(itemLi);
+    }
+};
+liImages();
+container.append(fragment)
+console.log(container);
+
+//Listener
+container.addEventListener("click", (e) => {
+  e.preventDefault();
+  let x = e.target;
+  let y = x.getAttribute("data-source");
+  let z = x.getAttribute("alt");
+	basicLightbox.create(`<img src = ${y} alt = ${z}>`).show() })
