46d12d962b4126d35c3ae807d11d8a2e6d205dad
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-01-22 19:25:51 +0200

    presentation

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+
+//Declaration
+const body = document.body
+const container =  document.querySelector(".gallery");
+const fragment = document.createDocumentFragment();
+let max = galleryItems.length;
+
+//Function
+function liImages()
+{
+  for(let i = 0; i < max; i++)
+    {let itemLi = document.createElement("li");
+     itemLi.classList.add("gallery__item");
+     let itemA = document.createElement("a");
+     itemA.classList.add("gallery__link");
+     let liImg = document.createElement("img");
+     liImg.classList.add("gallery__image");
+
+  let basic = galleryItems[i];
+  for(let item of galleryItems){
+     liImg.setAttribute("src", `${basic.preview}`);
+     liImg.setAttribute("alt", `${basic.description}`);}
+     itemA.setAttribute("href", `${basic.original}`);
+     itemA.setAttribute("title", `${basic.description}`);
+    
+
+     itemLi.append(itemA);
+     itemA.append(liImg);
+     fragment.append(itemLi);
+    }
+};    
+liImages();
+container.append(fragment)
+
+
+//Listener
+
+container.addEventListener("click", (event) => {
+  event.preventDefault();
+  let presentation = new SimpleLightbox('.gallery a', { /* options */ });})
+
+
+
+
+
